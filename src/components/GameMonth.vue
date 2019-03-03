af5e96a657a5a7ee79c1f3e511c41be4fd1cf269
Margo <luckinamargo@gmail.com>
2019-03-03 13:32:06 +0700

    add video carousel

--- src/components/GameMonth.vue
+++ src/components/GameMonth.vue
@@ -1,18 +1,40 @@
 <template>
-    <div>{{video}}
-    </div>
+    <carousel class="carousel">
+        <slide v-for="item in video">
+            <video class="video" controls>
+                <source :src="item" type="video/mp4">
+            </video>
+        </slide>
+    </carousel>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
+    components: {
+        Carousel,
+        Slide
+    },
     props: {
         video: {
             type: Array,
             default: () => []
         }
     },
-    created() {
-        console.log('GAMES! ', this.video)
+    mounted() {
+        console.log('VIDEO!!! ', this.video)
     }
 }
 </script>
+
+<style scoped>
+.carousel {
+    width: 900px;
+    margin: 150px auto 0;
+}
+.video {
+   width: 400px;
+   margin: 0 5px;
+}
+</style>
