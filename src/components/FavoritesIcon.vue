f9835cb2d17b6da0a290c69ddf8fa0ed2e676921
Margo <luckinamargo@gmail.com>
2019-03-08 21:35:56 +0700

    add favorites icon

---
+++ src/components/FavoritesIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <img v-if="isChecked" class="icon" src="../assets/favorites_checked.png"/>
+    <img v-else class="icon" src="../assets/favorites.png"/>
+</template>
+
+<script>
+export default {
+    props: {
+        isChecked: {
+            /** Является ли элемент выбранным */
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        /** путь до картинки */
+        imgSrc() {
+            return this.isChecked
+            ? '/img/favorites_checked.png'
+            : './img/favorites.png'
+        }
+    },
+}
+</script>
+
+<style scoped>
+.icon {
+    width: 20px;
+    height: 20px;
+}
+</style>
